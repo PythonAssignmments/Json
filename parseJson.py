d061d8f37de8744924d2a31cfb90c289643b956c
yogesh.bharate001 <yogesh.bharate@synerzip.com>
2015-07-14 18:28:10 +0530

    Added code to Create output directories as well as header and cpp files as per parse json values.

--- parseJson.py
+++ parseJson.py
@@ -1,17 +1,30 @@
 # This file is use to parse the json which loaded using file
+import os
 import json
 
 # read the file contents
 try:
 	# fp = open(raw_input())
 	# fileStr = fp.read()
+	outputDir = 'output'
+	hxxDir = outputDir+'/hxx'
+	cxxDir = outputDir+'/cpp'
+	if not os.path.exists(outputDir):
+		os.mkdir(outputDir)
+		if not os.path.exists(hxxDir):
+			os.makedirs(hxxDir)
+		if not os.path.exists(cxxDir):
+			os.makedirs(cxxDir)
+			
 	with open('jsonData.json') as json_file:
 		json_data = json.load(json_file)
 		for data in json_data:
+			# print json_data[data]
 			print data
+			headerFiles = open(hxxDir+'/'+data+".hxx", 'a')
+			cxxFiles = open(cxxDir+'/'+data+".cxx", 'a')
 
-		for data1 in data[1]:
-			print data1
+
 		# dict1 = []
 		# dict2 = {}
 		# for resp_item in form_this["value"]:
